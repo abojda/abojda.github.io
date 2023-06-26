f33176992d632abf9196f6154166f9ed13ebfb62
Aleksander Bojda <alebojd@gmail.com>
2023-06-26 22:20:00 +0200

    Add IP handling to jekyll docker script

--- jekyll_serve_docker.sh
+++ jekyll_serve_docker.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-docker run -v $(pwd):/site -p 4000:4000 -it ruby_jekyll bash -c 'cd site && jekyll serve --host 0.0.0.0'
+
+if [ -z "$1" ]
+then
+  # Use localhost by default
+  IP=127.0.0.1
+  echo -e "Serving Jekyll on localhost\n"
+else
+  IP=$1
+  echo -e "Serving Jekyll on $IP\n"
+fi
+
+docker run -v $(pwd):/site -p $IP:4000:4000 -it ruby_jekyll bash -c 'cd site && jekyll serve --host 0.0.0.0'
